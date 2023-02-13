07b7be3933213106c230a6984df597ec32f99cea
Matéo Moreau <devpleven@gmail.com>
2023-02-13 22:25:30 +0100

    Adding Files

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityRef = document.getElementById("city");
+
+//Function to fetch weather details from api and display them
+let getWeather = () => {
+  let cityValue = cityRef.value;
+  //If input field is empty
+  if (cityValue.length == 0) {
+    result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
+  }
+  //If input field is NOT empty
+  else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=8b88927667ef47db7f8246809bd6cf0a&units=metric`;
+    //Clear the input field
+    cityRef.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      //If city name is valid
+      .then((data) => {
+        result.innerHTML = `
+        <h2>${data.name}</h2>
+        </br>
+        <h1>${data.main.temp} &#176;</h1>
+        </br>
+        <h4 class="weather">${data.weather[0].main} (${data.weather[0].description})</h4>
+        <div class="temp-container">
+            <div>
+                <h4 class="title">min</h4>
+                <h4 class="temp">${data.main.temp_min}&#176;</h4>
+            </div>
+            <div>
+                <h4 class="title">max</h4>
+                <h4 class="temp">${data.main.temp_max}&#176;</h4>
+            </div>
+        </div>
+        `;
+      })
+      //If city name is NOT valid
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">City not found</h3>`;
+      });
+  }
+};
+searchBtn.addEventListener("click", getWeather);
+window.addEventListener("load", getWeather);
